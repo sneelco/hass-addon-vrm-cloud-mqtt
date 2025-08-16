e8404ff59fa33eab4d7bf24d76d54c3df5d4eb79
Scott Neel <scott@neel.net>
2025-08-16 14:17:27 -0700

    * add token name

--- hass_entry.sh
+++ hass_entry.sh
@@ -11,6 +11,8 @@ export VRM_USERNAME=$(bashio::config 'vrm_username')
 export VRM_PASSWORD=$(bashio::config 'vrm_password')
 export VRM_SITE_ID=$(bashio::config 'vrm_site_id')
 export VRM_POLL_INTERVAL=$(bashio::config 'vrm_poll_interval')
+export VRM_TOKEN_NAME=$(bashio::config 'vrm_token_name')
+export VRM_DEBUG=$(bashio::config 'vrm_debug')
 
 
   uv run run_poller.py
--- vrm_cloud_mqtt/config.py
+++ vrm_cloud_mqtt/config.py
@@ -8,7 +8,10 @@ class Settings(BaseSettings):
     """Application settings loaded from environment variables."""
 
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", case_sensitive=False, env_prefix="VRM_"
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        env_prefix="VRM_",
     )
 
     username: str = Field(..., description="Username for authentication")
